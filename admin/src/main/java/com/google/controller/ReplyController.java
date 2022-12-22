e0b603bbfff0fdddfa0f5997656f0440d987949f
BigData19 <BigData19@DESKTOP-8EA2US3>
2022-12-22 15:45:51 +0900

    최종

--- admin/src/main/java/com/google/controller/ReplyController.java
+++ admin/src/main/java/com/google/controller/ReplyController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.google.domain.Criteria;
 import com.google.domain.ReplyPageDTO;
@@ -23,7 +24,7 @@ import com.google.service.ReplyService;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j;
 
-@Controller
+@RestController
 @RequestMapping("/replies/*")
 @Log4j
 @AllArgsConstructor
